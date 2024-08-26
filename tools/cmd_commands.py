ec889470e034d64c223cb5384d6f455a761f9f13
paulocfrossard <paulo.frossard@proton.me>
2024-08-26 17:46:02 -0300

    refactor: improve command_exec function
    - Adds input validation for 'command' and 'target' parameters.

--- tools/cmd_commands.py
+++ tools/cmd_commands.py
@@ -1,15 +1,19 @@
 import subprocess
 
 
-def command_generic(command, target, args):
-    print(f"Iniciando {command}...")
+def command_exec(command, target, args, type):
+    print('\n'+('-'*7)+'Iniciando'+('-'*7))
     full_command = f"{command} {target} {args}"
-    status = subprocess.call(full_command, shell=True)
-    return status
-
-
-def command_output(command, target, args):
-    print("Iniciando " + command + "...")
-    full_command = f"{command} {target} {args}"
-    status = subprocess.run(full_command, shell=True, capture_output=True, text=True, check=True)
+    print(f" {full_command}...")
+    # 0 generic, 1 verbose, 2 simple
+    if type == 0:
+        print(f"Type {type}")
+        status = subprocess.run(full_command, shell=True)
+    elif type == 1:
+        print(f"Type {type}")
+        status = subprocess.run(full_command, shell=True, capture_output=True, text=True, check=True)
+    else:
+        print(f"Type {type}")
+        status = subprocess.run(command, shell=True)
+    print('\n'+('-'*7)+'Finalizado'+('-'*7))
     return status
