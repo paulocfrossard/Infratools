4a274e943ddb3721890822bd5463df741e478d8a
paulocfrossard <paulo.frossard@proton.me>
2024-08-28 11:38:01 -0300

    refactor: improve and simplify ping_tools and ip_refresh functions
    - Enhanced and simplified the ping_tools and ip_refresh functions.
    - Added the use of return_format for better output handling.

--- tools/network.py
+++ tools/network.py
@@ -1,23 +1,20 @@
 import subprocess
 
-from tools.cmd_commands import command_generic, command_output
+from tools.cmd_commands import command_exec
+from tools.formart import return_format
 
 
 def ping_tools(ips, dominio):
     ip_status = []
     for ip in ips:
-        status = command_generic("ping", ip, "-n 10")
-        status = [status, ip]
+        status = command_exec("ping", ip, "-n 10", 0)
+        satus_new = return_format(status)
+        status = [satus_new, ip]
         ip_status.append(status)
-    print(ip_status)
-    dominio_status = command_generic("ping", dominio, "-n 4")
+    dominio_status = command_exec("ping", dominio, "-n 10", 0)
     return ip_status, dominio_status
 
 
 def ip_refresh():
-    ip_release = command_generic("ipconfig ", "", "/release")
-    ip_renew = command_generic("ipconfig ", "", "/renew")
-
-
-def tracert_interno():
-    print('')
+    ip_release = command_exec("ipconfig", "", "/release", 0)
+    ip_renew = command_exec("ipconfig", "", "/renew", 0)
