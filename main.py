a9cb631d3a5c4c2d008e60013b337c58aa47eca8
paulocfrossard <paulo.frossard@proton.me>
2024-08-28 11:31:25 -0300

    fix: LICENSE

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# GNU GENERAL PUBLIC LICENSE 3
+# CC BY-NC-SA 4.0
 # Create the config .ini file in the executable folder, in case it has not been compiled in the folder of main.py
 # Github: https://github.com/paulocfrossard/Infratools/tree/main
 # READ readme.md for config
