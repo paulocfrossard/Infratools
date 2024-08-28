6f01589092d7da63e7f15d789278efef3d650560
paulocfrossard <paulo.frossard@proton.me>
2024-08-28 11:20:51 -0300

    refactor: improve code execution and recovery functions
    - Refactors the functions responsible for code execution and data recovery to enhance efficiency and robustness.
    - Changes the output format to make it clearer and more readable for users.
    - Creates a utility class import system to better organize the code and facilitate maintenance.
    
    These improvements aim to enhance user experience and code maintainability, making it more efficient and easier to understand.

--- tools/recovery.py
+++ tools/recovery.py
@@ -1,14 +1,16 @@
-import os
-import subprocess
+import re
+
+from tools.cmd_commands import command_exec
+from tools.formart import return_format
 
 
 def os_recovery():
-    def generic_command(command):
-        print("Iniciando " + command + "...")
-        status = subprocess.call(command, shell=True)
-        return status
-
-    sfc = generic_command("sfc /scannow ")
-    chkdsk = generic_command("echo s | chkdsk C: /f")
-    generic_command("cls")
-    return sfc, chkdsk
+    sfc = command_exec("sfc", "", "/scannow", 0)
+    chkdsk = command_exec("echo s | chkdsk C: /f", "", "", 2)
+    dism_check = command_exec("dism", "", "/online /cleanup-image /CheckHealth", 1)
+    dism_restore = command_exec("dism", "", "/online /cleanup-image /restorehealth", 1)
+    chkdsk = return_format(chkdsk)
+    sfc = return_format(sfc)
+    dism_check = return_format(dism_check)
+    dism_restore = return_format(dism_restore)
+    return [0], [3], [0], [0]
