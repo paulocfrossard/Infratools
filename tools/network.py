a6336164e32b0dca06092cd3277308a08b32175d
Paulo C Frossard <paulocorreafrossard@gmail.com>
2026-03-19 12:46:07 -0300

    Split funcs

--- tools/network.py
+++ tools/network.py
@@ -4,16 +4,18 @@ from tools.cmd_commands import command_exec
 from tools.formart import return_format
 
 
-def ping_tools(ips, dominio):
+def internal_dns_status(ips):
     ip_status = []
     for ip in ips:
         status = command_exec("ping", ip, "-n 10", 0)
         satus_new = return_format(status)
         status = [satus_new, ip]
         ip_status.append(status)
-    dominio_status = command_exec("ping", dominio, "-n 10", 0)
-    return ip_status, dominio_status
+    return ip_status
 
+def internal_domain_status(domain):
+    domain_status = command_exec("ping", domain, "-n 10", 0)
+    domain_route = return_format(domain_status) # Fazer
 
 def ip_refresh():
     ip_release = command_exec("ipconfig", "", "/release", 0)
